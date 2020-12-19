34620dfff690a25cfdca9400538f98ec5daf8b70
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-12-20 01:12:27 +0200

    initial version of app is almost done, the General page (home page) works fine (its slow, nothing is cached), still got alot to work on...

---
+++ lib/config.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const mainBlack = Color.fromRGBO(38, 38, 38, 1);
+const secondaryBlack = Color.fromRGBO(130, 130, 130, 1);
+const textColor = Colors.white;
+const mainRed = Color.fromRGBO(242, 93, 93, 1);
+const textStyle = TextStyle(
+  color: textColor,
+);
+const highlightedTextStyle = TextStyle(
+  color: mainRed,
+  fontSize: 17
+);
+
+const String BACKEND = 'http://localhost:5000';
+//const String BACKEND = 'https://trackifyapp.net';
+const String PASSWORD = "lion1230";
+const String USERNAME = "mahmoodsheikh36";
